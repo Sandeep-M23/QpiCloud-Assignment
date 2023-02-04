05ae31081ec04f4cd6969c5024a6fc535fb6595f
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2023-02-04 19:12:15 +0530

    Made Dashboard

---
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import SocketIOClient from "socket.io-client";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import SimpleLineChart from "../../components/Charts/SimpleLineChart";
+import SimpleBarChart from "../../components/Charts/SimpleBarChart";
+import SimplePieChart from "../../components/Charts/SimplePieChart";
+import SimpleAreaChart from "../../components/Charts/SimpleAreaChart";
+import Spinner from "../../components/Spinner/Spinner";
+import "./Dashboard.scss";
+
+interface ChartData {
+  [key: string]: any;
+}
+
+interface PieData {
+  name: string;
+  value: number;
+}
+
+const Dashboard: React.FC = () => {
+  const [lineData, setLineData] = useState<ChartData[]>([]);
+  const [pieData, setPieData] = useState<PieData[]>([]);
+  const [areaData, setAreaData] = useState<ChartData[]>([]);
+  const [barData, setBarData] = useState<ChartData[]>([]);
+  const [loading,setLoading] = useState<Boolean>(true);
+
+
+  useEffect(() => {
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+    
+    const socket = SocketIOClient("http://127.0.0.1:4001/");
+
+    socket.on("data", (b,l,a,p) => {
+      setBarData(b);
+      setLineData(l);
+      setAreaData(a);
+      setPieData(p);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [barData]);
+
+  return (
+    <>
+      <Sidebar />
+      <div className="home">
+        {loading ? (
+          <Spinner />
+        ) : (
+          <>
+            <SimpleBarChart data={barData} />
+            <SimpleAreaChart data={areaData} />
+            <SimpleLineChart data={lineData} />
+            <SimplePieChart data={pieData} />
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
